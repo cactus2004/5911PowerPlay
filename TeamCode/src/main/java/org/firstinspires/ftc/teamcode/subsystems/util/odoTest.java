75688fc896d530b35a8fc25d0675d4dc490b1a86
cactus2004 <107892919+cactus2004@users.noreply.github.com>
2023-02-26 19:06:35 -0700

    moved stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/odoTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsystems.util;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.util.Encoder;
+
+@TeleOp
+public class odoTest extends LinearOpMode {
+
+    private Encoder leftEncoder, rightEncoder, frontEncoder;
+
+    public void runOpMode() {
+
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "liftLeft"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightEncoder"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontEncoder"));
+
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+
+
+            int leftPos = leftEncoder.getCurrentPosition();
+            int rightPos = rightEncoder.getCurrentPosition();
+            int frontPos = frontEncoder.getCurrentPosition();
+
+            telemetry.addData("left", leftPos);
+            telemetry.addData("right", rightPos);
+            telemetry.addData("front", frontPos);
+
+
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/slideTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/util/slideTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode.subsystems.util;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
